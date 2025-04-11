67e17b053a32b07cb2e941a66c951e55fa301c07
Juma Allan <allanjuma@gmail.com>
2025-04-11 16:03:52 +0300

    setting up ktlint (#6)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,18 @@
 plugins {
+    // Applied to all sub-modules
+    alias(libs.plugins.ktlint)
+
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+}
+
+tasks.create("clean", Delete::class.java) {
+    delete(rootProject.layout.buildDirectory)
+}
+
+ktlint {
+    android = true
 }
